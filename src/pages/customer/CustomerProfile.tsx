0301f7cee9a58e5bf05442a6b316e7369993a822
Rifat Sarker <rifatswd@gmail.com>
2025-02-01 11:41:26 +0600

    Enhance CustomerProfile component; update layout with responsive design and display user role, and add product and order management types to index

--- src/pages/customer/CustomerProfile.tsx
+++ src/pages/customer/CustomerProfile.tsx
@@ -1,15 +1,16 @@
-import { Form, Input, Button, Card, Avatar, Typography } from "antd";
+import { Form, Input, Button, Card, Avatar, Typography, Row, Col } from "antd";
 import { useUpdatePasswordMutation } from "../../redux/features/customer/customerApi";
 import { useAppSelector } from "../../redux/hooks";
 import { toast } from "sonner";
 import { FieldValues } from "react-hook-form";
+import { selectCurrentUser } from "../../redux/features/auth/authSlice";
 
 const { Title, Text } = Typography;
 
 const CustomerProfile = () => {
   const [updatePassword] = useUpdatePasswordMutation();
   const [form] = Form.useForm();
-  const user = useAppSelector((state) => state.auth.user);
+  const user = useAppSelector(selectCurrentUser);
 
   if (!user) {
     return <p>Loading...</p>;
@@ -45,8 +46,6 @@ const CustomerProfile = () => {
       style={{
         display: "flex",
         justifyContent: "center",
-        alignItems: "center",
-        minHeight: "100vh",
         padding: "20px",
         backgroundColor: "#f0f2f5",
       }}
@@ -54,74 +53,75 @@ const CustomerProfile = () => {
       <Card
         style={{
           width: "100%",
-          maxWidth: "600px",
+          maxWidth: "800px",
           borderRadius: "8px",
           boxShadow: "0 2px 8px rgba(0, 0, 0, 0.15)",
           padding: "20px",
         }}
       >
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar
-            src={
-              "https://i.ibb.co.com/MkbyPSHB/360-F-229758328-7x8jw-Cwjt-BMm-C6rg-Fz-LFh-Zo-Ep-Lob-B6-L8.jpg"
-            }
-            size={80}
-          />
-          <Title level={3} style={{ margin: "20px 0 10px" }}>
-            {user.name}
-          </Title>
-          <Text type="secondary">{user.email}</Text>
-        </div>
-        <div style={{ marginTop: "30px", width: "100%" }}>
-          <h1 style={{ textAlign: "center", marginBottom: "20px" }}>
-            Change Password
-          </h1>
-          <Form
-            layout="vertical"
-            onFinish={handlePasswordChange}
-            form={form}
-            style={{ width: "100%" }}
-          >
-            <Form.Item
-              name="currentPassword"
-              label="Current Password"
-              rules={[
-                { required: true, message: "Enter your current password" },
-              ]}
-            >
-              <Input.Password placeholder="Current password" />
-            </Form.Item>
-            <Form.Item
-              name="newPassword"
-              label="New Password"
-              rules={[{ required: true, message: "Enter a new password" }]}
-            >
-              <Input.Password placeholder="New password" />
-            </Form.Item>
-            <Form.Item
-              name="confirmPassword"
-              label="Confirm New Password"
-              rules={[{ required: true, message: "Confirm your new password" }]}
-            >
-              <Input.Password placeholder="Confirm password" />
-            </Form.Item>
-            <Button
-              type="primary"
-              className="secondary-bg"
-              htmlType="submit"
-              block
-              style={{ fontSize: "16px", height: "40px", color: "black" }}
+        <Row gutter={[24, 24]} align="middle">
+          <Col xs={24} md={10} style={{ textAlign: "center" }}>
+            <Avatar
+              src={
+                "https://i.ibb.co.com/MkbyPSHB/360-F-229758328-7x8jw-Cwjt-BMm-C6rg-Fz-LFh-Zo-Ep-Lob-B6-L8.jpg"
+              }
+              size={80}
+            />
+            <Title level={4} style={{ margin: "15px 0 5px" }}>
+              {user.email}
+            </Title>
+            <Text type="secondary">{user.role}</Text>
+          </Col>
+
+          <Col xs={24} md={14}>
+            <Title
+              level={4}
+              style={{ textAlign: "center", marginBottom: "20px" }}
             >
               Change Password
-            </Button>
-          </Form>
-        </div>
+            </Title>
+            <Form
+              layout="vertical"
+              onFinish={handlePasswordChange}
+              form={form}
+              style={{ width: "100%" }}
+            >
+              <Form.Item
+                name="currentPassword"
+                label="Current Password"
+                rules={[
+                  { required: true, message: "Enter your current password" },
+                ]}
+              >
+                <Input.Password placeholder="Current password" />
+              </Form.Item>
+              <Form.Item
+                name="newPassword"
+                label="New Password"
+                rules={[{ required: true, message: "Enter a new password" }]}
+              >
+                <Input.Password placeholder="New password" />
+              </Form.Item>
+              <Form.Item
+                name="confirmPassword"
+                label="Confirm New Password"
+                rules={[
+                  { required: true, message: "Confirm your new password" },
+                ]}
+              >
+                <Input.Password placeholder="Confirm password" />
+              </Form.Item>
+              <Button
+                type="primary"
+                block
+                htmlType="submit"
+                style={{ fontSize: "16px", height: "40px", color: "black" }}
+              >
+                Change Password
+              </Button>
+            </Form>
+          </Col>
+        </Row>
       </Card>
     </div>
   );
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,4 @@
-export * from './navbar.type'
-export * from './global'
+export * from "./navbar.type";
+export * from "./global";
+export * from "./productManagement.type";
+export * from "./orderManagement.type";
