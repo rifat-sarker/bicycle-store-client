37cb2a64aecd620e86fda082ca31e48779fd21cd
rifat-sarker <rifatswd@gmail.com>
2025-05-24 11:50:18 +0600

    Update base API URL in baseApi.ts to point to the production server

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,7 +2,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
 
 const baseQuery = fetchBaseQuery({
-
+  // http://localhost:5000
+  //
   baseUrl: "https://bicycle-store-backend-livid.vercel.app/api",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
