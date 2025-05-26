b878e98f8e2ab02060d3f976543df0b565774ea8
rifat-sarker <rifatswd@gmail.com>
2025-05-26 23:05:39 +0600

    feat: update API base URL to use environment variable; improve authorization header format

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,13 +2,12 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
 
 const baseQuery = fetchBaseQuery({
-  // https://bicycle-store-backend-livid.vercel.app
-  baseUrl: "http://localhost:5000/api",
+  baseUrl: import.meta.env.VITE_BASE_URL,
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
     if (token) {
-      headers.set("authorization", `${token}`);
+      headers.set("authorization", `Bearer ${token}`);
     }
     return headers;
   },
