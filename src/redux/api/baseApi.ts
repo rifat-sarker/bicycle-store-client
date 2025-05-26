6a87ffd2e5cde28a82d2d32c31a4ae5c278aa28c
rifat-sarker <rifatswd@gmail.com>
2025-05-26 23:47:32 +0600

    feat: update base API URL to a hardcoded value; modify authorization header format

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,12 +2,13 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: import.meta.env.VITE_BASE_URL,
+  // https://bicycle-store-backend-livid.vercel.app
+  baseUrl: "https://bicycle-store-backend-livid.vercel.app/api",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
     if (token) {
-      headers.set("authorization", `Bearer ${token}`);
+      headers.set("authorization", `${token}`);
     }
     return headers;
   },
